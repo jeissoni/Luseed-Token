c8d6820a58c95162c9321810b7a3ac60637cd649
jeissoni <jeissoni_22@hotmail.com>
2026-03-25 22:46:00 -0500

    Add LuseedInvestment address to environment configuration
    - Added the VITE_LUSEED_INVESTMENT_ADDRESS variable to the vite-env.d.ts file, enhancing the environment configuration for the LuseedInvestment contract.
    - This update improves the project's ability to manage and interact with the LuseedInvestment contract within the application.

--- FrontEnd/src/vite-env.d.ts
+++ FrontEnd/src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_PROMISSORY_NOTE_ADDRESS: string;
   readonly VITE_LUSEED_TOKEN_ADDRESS: string;
   readonly VITE_LUSEED_DAO_ADDRESS: string;
+  readonly VITE_LUSEED_INVESTMENT_ADDRESS: string;
   readonly VITE_USDC_ADDRESS: string;
 }
 
